f4d23c09cb9fb80967efc0b5ec19bf0ef133f967
GaspardPO <gaspard.pointeau@gmail.com>
2020-12-01 22:59:56 +0100

    each planks has a length. create a heap adding some planks.

--- rust/src/calepinage.rs
+++ rust/src/calepinage.rs
@@ -2,13 +2,15 @@
 pub struct Deck { pub length: usize }
 
 #[derive(Debug, PartialEq, Clone)]
-pub struct Plank { pub id:usize}
+pub struct Plank { pub length:usize}
 
+#[derive(Default)]
 pub struct PlankHeap { planks: Vec<Plank>}
-impl PlankHeap {
 
-    pub fn new(size:usize) -> Self {
-        let planks = (0..size).map(|id| Plank { id }).collect();
+impl PlankHeap {
+    pub fn add(self, count: usize, length: usize) -> Self {
+        let mut planks : Vec<Plank>= (0..count).map(|id| Plank { length: length }).collect();
+        planks.extend_from_slice(&self.planks);
         PlankHeap { planks }
     }
 }
--- rust/tests/calepinage_test.rs
+++ rust/tests/calepinage_test.rs
@@ -5,33 +5,33 @@ mod calepinage_test {
     #[test]
     fn should_return_planks_when_deck_is_really_small() {
         let deck = Deck {length : 1};
-        let plank_heap = PlankHeap::new(1);
+        let plank_heap = PlankHeap::default().add(1,1);
 
         let actual = calepine(plank_heap, deck);
 
-        let expected = vec![vec![Plank { id: 0 }]];
+        let expected = vec![vec![Plank { length: 1 }]];
         assert_eq!(expected, actual);
     }
 
     #[test]
     fn should_return_list_of_2_planks() {
         let deck = Deck {length : 2};
-        let plank_heap = PlankHeap::new(2);
+        let plank_heap = PlankHeap::default().add(2,1);
 
         let actual = calepine(plank_heap, deck);
 
-        let expected = vec![vec![Plank {id:0}, Plank {id:1}]];
+        let expected = vec![vec![Plank { length:1}, Plank { length:1}]];
         assert_eq!(expected, actual);
     }
 
     #[test]
     fn should_use_only_one_plank_if_does_not_need_more() {
         let deck = Deck {length : 1};
-        let plank_heap = PlankHeap::new(2);
+        let plank_heap = PlankHeap::default().add(2,1);
 
         let actual = calepine(plank_heap, deck);
 
-        let planks_count = actual.iter().flatten().count();
-        assert_eq!(1, planks_count);
+        let expected = vec![vec![Plank { length: 1 }]];
+        assert_eq!(expected, actual);
     }
 }
